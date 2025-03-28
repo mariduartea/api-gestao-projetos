1160361448d8dfdf2fa5acbf642c5e540a99bf5b
Mariana Reboucas <marianarda14@gmail.com>
2025-03-28 18:02:34 -0300

    atualizando o test_db e incluindo o campo uppdate_at na tabela User

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 
 from fastapi_zero.app import app
@@ -22,3 +25,23 @@ def session():
         yield session  # o yield delimita o setup, que roda antes do teste
 
     table_registry.metadata.drop_all(engine)  # teardown
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1),):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
