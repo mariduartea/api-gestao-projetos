13b61fc51745e3ce898452e96cc6003c63351251
Mariana Reboucas <marianarda14@gmail.com>
2025-07-09 19:55:44 -0300

    Adiciona cenário E2E para deletar usuário e verificar remoção em times

--- tests/e2e/feature/users.feature
+++ tests/e2e/feature/users.feature
@@ -18,8 +18,10 @@ Scenario: Update a user and verify that the change appears in the project list
     When the user changes their name
     Then the project must list the new name as a member
 
-# Scenario: Delete a user and verify that they no longer appear as a member of a team
-#     Given a random user is created
-#     And a random team is created with that user
-#     When the user is deleted
-#     Then the team must not list the deleted user as a member
+Scenario: Delete a user and verify that they no longer appear as a member of a team
+    Given a random user is created
+    And a random team is created with that user
+    And another random user is created
+    And the team list is updated with the new user
+    When the other user is deleted
+    Then the team must not list the deleted user as a member
---
+++ tests/utils/helpers.py
@@ -0,0 +1,71 @@
+from http import HTTPStatus
+
+from conftest import UserFactory
+from utils.fake_data import fake_project_name, fake_team_name, fake_user_data
+
+from task_flow.security import get_password_hash
+
+
+def create_random_user(session, context):
+    data = fake_user_data()
+    user = UserFactory(
+        username=data['username'],
+        email=data['email'],
+        password=get_password_hash(data['password']),
+    )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    context['user_id'] = user.id
+    context['username'] = data['username']
+    context['email'] = data['email']
+    context['password'] = data['password']
+    print('User created:', data['username'])
+    return context
+
+
+def authentication(client, context):
+    # Autentica o usuário
+    response = client.post(
+        '/auth/token',
+        data={'username': context['email'], 'password': context['password']},
+    )
+    assert response.status_code == HTTPStatus.OK
+    token = response.json()['access_token']
+    context['headers'] = {'Authorization': f'Bearer {token}'}
+
+
+def create_random_team(client, context):
+    team_name = fake_team_name()
+    context['team_name'] = team_name
+
+    # Cria o time via api
+    response = client.post(
+        '/teams/',
+        json={'team_name': team_name, 'user_list': [context['username']]},
+        headers=context['headers'],
+    )
+    assert response.status_code == HTTPStatus.CREATED, (
+        f'Error while creating team: {response.json()}'
+    )
+    print('Team created:', team_name)
+
+
+def create_random_project(client, context):
+    project_name = fake_project_name()
+    context['project_name'] = project_name
+
+    response = client.post(
+        '/projects/',
+        json={
+            'project_name': project_name,
+            'team_list': [context['team_name']],
+        },
+        headers=context['headers'],
+    )
+
+    assert response.status_code == HTTPStatus.CREATED, (
+        f'Error while creating project: {response.json()}'
+    )
